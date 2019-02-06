78f0dd54d16a5eceaf1f96ca50d80fe1d333fa30
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-06 16:12:04 -0700

    beginning state variable https://app.asana.com/0/1108517459409227/1108517459409230

--- php/Classes/Profile.php
+++ php/Classes/Profile.php
@@ -1,7 +1,40 @@
 <?php
+namespace abqtrails;
+
 /**
- * Created by PhpStorm.
- * User: Pure-Manage
- * Date: 2/6/2019
- * Time: 3:44 PM
+ *
+ * This profile is used to input and view data and photos on ABQ Trails web application
+ *
+ * @author Cassandra Romero <cromero278@cnm.edu>
+ *
  */
+
+class Profile {
+
+	/**
+	 * id for user profile, this is a primary key
+	 * @var Uuid $profileId
+	 */
+	private $profileId;
+	/**
+	 * One-time activation token used for profile account creation
+	 * @var $profileActivationToken
+	 */
+	private $profileActivationToken;
+	/**
+	 * url for author avatar (photo)
+	 * @var $profileAvatarUrl
+	 */
+	private $profileAvatarUrl;
+/**
+ * profile email address, must be unique
+ * @var $profileEmail
+ */
+	private $profileEmail;
+/**
+ * encrypted profile password
+ * @var $profileHash
+ */
+	private $profileHash;
+}
+
