c5cf9ce40dd50fbd22c98b7f5764e6e9ff6f5d14
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-06 16:29:32 -0700

    beginning constructor code https://app.asana.com/0/1108517459409227/1108517459409230

--- php/Classes/Profile.php
+++ php/Classes/Profile.php
@@ -56,8 +56,20 @@ class Profile {
  * @param string|Uuid $newProfileId id for this profile
  * @param string $newProfileActivationToken string containing activation token for account creation
  * @param string $newProfileAvatarUrl string containing url of profile avatar
- * @parma string $newProfileEmail unique string containing unique profile email address
- */
-
+ * @param string $newProfileEmail unique string containing unique profile email address
+ * @param string $newProfileFirstName string containing profile user first name
+ * @param string $newProfileHash string containing hashed profile password
+ * @param string $newProfileLastName string containing profile user last name
+ * @param string $newProfileUsername string containing unique profile username
+ * @throws \InvalidArgumentException if data types are not valid
+ * @throws \RangeException if data values are out of bounds
+ * @throws \TypeError if data types violate type hints
+ * @throws \Exception if some other exception occurs
+ **/
+public function __construct($newProfileId, $newProfileActivationToken, $newProfileAvatarUrl, $newProfileEmail, $newProfileFirstName, $newProfileHash, $newProfileLastName, $newProfileUsername) {
+	try {
+		$this->setProfileId($newProfileId);
+	}
+	}
 }
 
