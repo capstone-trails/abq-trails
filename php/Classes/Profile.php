65a55accae4737cafdfc90406a5def0effc60511
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-06 16:21:36 -0700

    completed state variable https://app.asana.com/0/1108517459409227/1108517459409230

--- php/Classes/Profile.php
+++ php/Classes/Profile.php
@@ -8,23 +8,21 @@ namespace abqtrails;
  * @author Cassandra Romero <cromero278@cnm.edu>
  *
  */
-
 class Profile {
-
-	/**
-	 * id for user profile, this is a primary key
-	 * @var Uuid $profileId
-	 */
+/**
+ * id for user profile, this is a primary key
+ * @var Uuid $profileId
+ */
 	private $profileId;
-	/**
-	 * One-time activation token used for profile account creation
-	 * @var $profileActivationToken
-	 */
+/**
+ * One-time activation token used for profile account creation
+ * @var $profileActivationToken
+ */
 	private $profileActivationToken;
-	/**
-	 * url for author avatar (photo)
-	 * @var $profileAvatarUrl
-	 */
+/**
+ * url for author avatar (photo)
+ * @var $profileAvatarUrl
+ */
 	private $profileAvatarUrl;
 /**
  * profile email address, must be unique
@@ -32,9 +30,34 @@ class Profile {
  */
 	private $profileEmail;
 /**
+ * profile user's first name
+ * @var $profileFirstName
+ */
+	private $profileFirstName;
+/**
  * encrypted profile password
  * @var $profileHash
- */
+*/
 	private $profileHash;
+/**
+ * profile user's last name
+ * @var $profileLastName
+ */
+	private $profileLastName;
+/**
+ * profile username, must be unique
+ * @var $profileUsername
+ */
+	private $profileUsername;
+
+/**
+ * constructor for Author
+ *
+ * @param string|Uuid $newProfileId id for this profile
+ * @param string $newProfileActivationToken string containing activation token for account creation
+ * @param string $newProfileAvatarUrl string containing url of profile avatar
+ * @parma string $newProfileEmail unique string containing unique profile email address
+ */
+
 }
 
