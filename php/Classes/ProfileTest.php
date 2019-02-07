05084cd4c596b8dd6d3467a3b6093205a03040c9
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-07 15:06:44 -0700

    updates to Profile unit test https://app.asana.com/0/1108517459409227/1108517459409230 ^this url was wrong in my several commits

--- php/Classes/ProfileTest.php
+++ php/Classes/ProfileTest.php
@@ -14,32 +14,47 @@ require_once("../../vendor/autoload.php");
  * @author Cassandra Romero <cromero278@cnm.edu>
  */
 class ProfileTest extends AbqTrailsTest {
-/**
- * valid profile id
- * @var string $VALID_PROFILE_ID
- */
+	/**
+	 * valid profile id
+	 * @var string $VALID_PROFILE_ID
+	 */
 	protected $VALID_PROFILE_ID = "nananananananana";
-/**
- * valid profile activation
- * @var string $VALID_PROFILE_ACTIVATION_TOKEN
- */
+	/**
+	 * valid profile activation
+	 * @var string $VALID_PROFILE_ACTIVATION_TOKEN
+	 */
 	protected $VALID_PROFILE_ACTIVATION_TOKEN = "hahahahahahahahahahahahahahahaha";
-/**
- * valid profile avatar url
- * @var string $VALID_PROFILE_AVATAR_URL
- */
+	/**
+	 * valid profile avatar url
+	 * @var string $VALID_PROFILE_AVATAR_URL
+	 */
 	protected $VALID_PROFILE_AVATAR_URL = "https://www.testavatarurlabqtrails.co";
-/**
- * valid profile email address
- * @var string $VALID_PROFILE_EMAIL
- */
-	protected $VALID_PROFILE_EMAIL = "newusertest@abqtrails.com";
-/**
- * valid profile first name
- * @var string $VALID_PROFILE_FIRST_NAME
- */
+	/**
+	 * updated profile avatar url
+	 * @var string $VALID_PROFILE_AVATAR_URL_2
+	 */
+	protected $VALID_PROFILE_AVATAR_URL_2 = "https://www.igotanewavatar.com/1234";
+	/**
+	 * valid profile email address
+	 * @var string $VALID_PROFILE_EMAIL
+	 */
+	protected $VALID_PROFILE_EMAIL = "newusertest@testtest.com";
+	/**
+	 * updated profile email
+	 * @var string $VALID_PROFILE_EMAIL_2
+	 */
+	protected $VALID_PROFILE_EMAIL_2 = "sameusernewemail@test.com";
+	/**
+	 * valid profile first name
+	 * @var string $VALID_PROFILE_FIRST_NAME
+	 */
 	protected $VALID_PROFILE_FIRST_NAME = "Nancy";
-/**
+	/**
+	 * updated first name
+	 * @var string $VALID_PROFILE_FIRST_NAME_2
+	 */
+	protected $VALID_PROFILE_FIRST_NAME_2 = "Fancy";
+	/**
  * valid profile hash
  * @var string $VALID_PROFILE_HASH
  */
