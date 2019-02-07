50502bd9cb5ad1c4c1d98028471dcd423ba0cafa
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-07 13:09:05 -0700

    constructor for trail tag https://app.asana.com/0/1108517459409227/1108517459409235

--- php/Classes/TrailTag.php
+++ php/Classes/TrailTag.php
@@ -17,4 +17,29 @@ class TrailTag {
  */
 	private $trailTagTrailId;
 /**
+ *this is the id that connects trail tag to the profile adding the tag this is a foreign key
+ */
+	private $trailTagProfileId;
+/**
+ * constructor for trailTag
+ * @param string | Uuid $newTrailTagTagId Id that connects tag and trail tag
+ * @param string | Uuid $newTrailTagTrailId Id that connects trail and trail tag
+ * @param string | Uuid $newTrailTagProfileId Id that connects profiles and trail tag
+ * @throws \InvalidArgumentException if data types are not valid
+ * @throws \RangeException if data values are out of bounds
+ * @throws \TypeError if data types violate type hints
+ * @throws \Exception if some other exception occurs
+ */
+	public function __construct($newTrailTagTagId, $newTrailTagTrailId, $newTrailTagProfileId) {
+		try {
+			$this->setTrailTagTagId($newTrailTagTagId);
+			$this->setTrailTagTrailId($newTrailTagTrailId);
+			$this->setTrailTagProfileId($newTrailTagProfileId);
+		}
+		catch(\InvalidArgumentException | \RangeException | \TypeError $exception){
+			$exceptionType = get_class(($exception));
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+	}
+
 }
