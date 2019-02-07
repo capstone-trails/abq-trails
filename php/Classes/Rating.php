df084a0edc424cb9b1b9fe0d2e1fb571f096c75b
Robert Dominguez <dominguez.1st@gmail.com>
2019-02-07 13:35:25 -0700

    Made the attributes. https://app.asana.com/0/1108517459409227/1108517459409236

--- php/Classes/Rating.php
+++ php/Classes/Rating.php
@@ -6,6 +6,32 @@ namespace abqtrails;
  *
  * @author Robert Dominguez <rdominguez45@cnm.edu
  **/
-class rating{
+class rating {
+	/**
+	 * id for this Rating; this is the primary key
+	 * @var Uuid $ratingId
+	 **/
+	private $ratingId;
+	/**
+	 * id for this Rating
+	 * @var Uuid $ratingProfileId
+	 **/
+	private $ratingProfileId;
+	/**
+	 * id for this Rating
+	 * @var Uuid $ratingTrailId
+	 **/
+	private $ratingTrailId;
+	/**
+	 * id for this Rating
+	 * @var Uuid $ratingDifficulty
+	 **/
+	private $ratingDifficulty;
+	/**
+	 * id for this Rating
+	 * @var Uuid $ratingDifficulty
+	 **/
+	private $ratingValue;
+
 
 }
