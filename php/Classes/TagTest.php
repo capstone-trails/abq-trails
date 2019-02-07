64d3b5f085c96670ad9020f0d2bf6d317dae88b1
Robert Dominguez <dominguez.1st@gmail.com>
2019-02-07 15:52:04 -0700

    Made the docblock and class for tag test. https://app.asana.com/0/1108517459409227/1108517459409233

--- php/Classes/TagTest.php
+++ php/Classes/TagTest.php
@@ -1,1 +1,25 @@
 <?php
+namespace abqtrails;
+
+use abqtrails/Tag;
+
+//grab the class under scrutiny
+require_once("autoload.php");
+
+//grab the uuid generator
+require_once("../../vendor/autoload.php");
+
+/**
+ * Full PHPUnit for the Tag class
+ *
+ * This is a complete PHPUnit test of the tweet class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see Tag
+ * @author Robert Dominguez
+ **/
+class TagTest extends  AbqTrailsTest {
+	/**
+	 * Profile that created
+	 **/
+}
