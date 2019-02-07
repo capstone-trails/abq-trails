ff87db837b5fd9c43a610e2e05118ed0ef3e3b8e
Ronald Luna <ronaldluna1@gmail.com>
2019-02-07 13:28:51 -0700

    created photo test php file https://app.asana.com/0/1108517459409227/1108517459409235

--- php/Classes/phototest.php
+++ php/Classes/phototest.php
@@ -105,3 +105,250 @@ abstract class photoTest extends TestCase {
 		return($this->getConnection()->getConnection());
 	}
 }
+
+<?php
+namespace Edu\Cnm\abq-trails\Test;
+
+use Edu\Cnm\abq-trails\{ProfileUser, Photo};
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/autoload.php");
+
+// grab the uuid generator
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+
+/**
+ * Full PHPUnit test for the photo class
+ *
+ * This is a complete PHPUnit test of the photo class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see photo
+ * @author Ronald Luna <rluna41@cnm.edu>
+ **/
+class TweetTest extends abq-trailsTest {
+	/**
+	 * ProfileUser that created the photo; this is for foreign key relations
+	 * @var ProfileUserId profileUserId
+	 **/
+	protected $profileUserId = null;
+
+
+	/**
+	 * valid profileUser Id to create the profileUserId object to own the test
+	 * @var $VALID_ID
+	 */
+	protected $VALID_PROFILE USER_ID;
+
+	/**
+	 * content of the photo
+	 * @var string $VALID_PHOTOURL
+	 **/
+	protected $VALID_PHOTOURL = "PHPUnit test passing";
+
+	/**
+	 * content of the updated photo
+	 * @var string $VALID_PHOTOURL2
+	 **/
+	protected $VALID_PHOTOURL2 = "PHPUnit test still passing";
+
+	/**
+	 * timestamp of the photo; this starts as null and is assigned later
+	 * @var \photoDateTime $VALID_PHOTODATETIME
+	 **/
+	protected $VALID_PHOTOURL = null;
+
+	/**
+	 * Valid timestamp to use as sunrisePhotoDateTime
+	 */
+	protected $VALID_SUNRISEDATETIME = null;
+
+	/**
+	 * Valid timestamp to use as sunsetPhotoDateTime
+	 */
+	protected $VALID_SUNSETDATETIME = null;
+
+	/**
+	 * create dependent objects before running each test
+	 **/
+	public final function setUp()  : void {
+		// run the default setUp() method first
+		parent::setUp();
+		$password = "abc123";
+		$this->VALID_PROFILE_ID = password_id($password, PASSWORD_ARGON2I, ["time_cost" => 384]);
+
+
+		// create and insert a Profile to own the test Tweet
+		$this->profileUserId = new ProfileUserId(generateUuidV4(), null,"@handle", "https://media.giphy.com/media/3og0INyCmHlNylks9O/giphy.gif", "test@phpunit.de",$this->VALID_PROFILE_HASH, "+12125551212");
+		$this->profileUserId->insert($this->getPDO());
+
+		// calculate the date (just use the time the unit test was setup...)
+		$this->VALID_PHOTODATETIME = new \photoDateTime();
+
+		//format the sunrise date time to use for testing
+		$this->VALID_SUNRISEDATETIME = new \photoDateTime();
+		$this->VALID_SUNRISEDATETIME->sub(new \DateTimeInterval("P10D"));
+
+		//format the sunset date time to use for testing
+		$this->VALID_SUNSETDATETIME = new\photoDateTime();
+		$this->VALID_SUNSETDATETIME->add(new \DateTmeInterval("P10D"));
+
+
+
+	}
+
+	/**
+	 * test inserting a valid photo and verify that the actual mySQL data matches
+	 **/
+	public function testInsertValidPhoto() : void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("photo");
+
+		// create a new photo and insert to into mySQL
+		$photoId = generateUuidV4();
+		$photo = new photo($photoId, $this->profileUserId->getProfileUserId(), $this->VALID_PHOTOURL, $this->VALID_PHOTODATETIME);
+		$photo->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoPhoto = photo::getphotoByPhotoId($this->getPDO(), $photo->getPhotoId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("photo"));
+		$this->assertEquals($pdoPhoto->getPhotoId(), $PhotoId);
+		$this->assertEquals($pdoPhoto->getPhotoProfileUserId(), $this->profile->getProfileUserId());
+		$this->assertEquals($pdoPhoto->getPhotoUrl(), $this->VALID_PHOTOURL);
+		//format the date too seconds since the beginning of time to avoid round off error
+		$this->assertEquals($pdoPhoto->getPhotoDateTime()->getTimestamp(), $this->VALID_PHOTODATETIME->getTimestamp());
+	}
+
+	/**
+	 * test inserting a photo, editing it, and then updating it
+	 **/
+	public function testUpdateValidPhoto() : void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("photo");
+
+		// create a new photo and insert to into mySQL
+		$potoId = generateUuidV4();
+		$photot = new photo($photoId, $this->profile->getProfileUserId(), $this->VALID_PHOTOURL, $this->VALID_PHOTODATETIME);
+		$photo->insert($this->getPDO());
+
+		// edit the Tweet and update it in mySQL
+		$photo->setPhotoUrl($this->VALID_PHOTOURL2);
+		$photo->update($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoTweet = photo::getphotoByPhotoId($this->getPDO(), $photo->getPhotoId());
+		$this->assertEquals($pdophoto->getPhotoId(), $photoId);
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("photo"));
+		$this->assertEquals($pdoPhoto->getPhotoProfileUserId(), $this->profile->getProfileUserId());
+		$this->assertEquals($pdoPhoto->getPhotoUrl(), $this->VALID_PHOTOURL2);
+		//format the date too seconds since the beginning of time to avoid round off error
+		$this->assertEquals($pdoPhoto->getPhotoDateTime()->getTimestamp(), $this->VALID_PHOTODATETIME->getTimestamp());
+	}
+
+
+	/**
+	 * test creating a photo and then deleting it
+	 **/
+	public function testDeleteValidPhoto() : void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("photo");
+
+		// create a new Tweet and insert to into mySQL
+		$tweetId = generateUuidV4();
+		$tweet = new Tweet($tweetId, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
+		$tweet->insert($this->getPDO());
+
+		// delete the Tweet from mySQL
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tweet"));
+		$tweet->delete($this->getPDO());
+
+		// grab the data from mySQL and enforce the Tweet does not exist
+		$pdoTweet = Tweet::getTweetByTweetId($this->getPDO(), $tweet->getTweetId());
+		$this->assertNull($pdoTweet);
+		$this->assertEquals($numRows, $this->getConnection()->getRowCount("tweet"));
+	}
+
+	/**
+	 * test inserting a Tweet and regrabbing it from mySQL
+	 **/
+	public function testGetValidTweetByTweetProfileId() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tweet");
+
+		// create a new Tweet and insert to into mySQL
+		$tweetId = generateUuidV4();
+		$tweet = new Tweet($tweetId, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
+		$tweet->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Tweet::getTweetByTweetProfileId($this->getPDO(), $tweet->getTweetProfileId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tweet"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\DataDesign\\Tweet", $results);
+
+		// grab the result from the array and validate it
+		$pdoTweet = $results[0];
+
+		$this->assertEquals($pdoTweet->getTweetId(), $tweetId);
+		$this->assertEquals($pdoTweet->getTweetProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoTweet->getTweetContent(), $this->VALID_TWEETCONTENT);
+		//format the date too seconds since the beginning of time to avoid round off error
+		$this->assertEquals($pdoTweet->getTweetDate()->getTimestamp(), $this->VALID_TWEETDATE->getTimestamp());
+	}
+
+	/**
+	 * test grabbing a Tweet by tweet content
+	 **/
+	public function testGetValidTweetByTweetContent() : void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tweet");
+
+		// create a new Tweet and insert to into mySQL
+		$tweetId = generateUuidV4();
+		$tweet = new Tweet($tweetId, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
+		$tweet->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Tweet::getTweetByTweetContent($this->getPDO(), $tweet->getTweetContent());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tweet"));
+		$this->assertCount(1, $results);
+
+		// enforce no other objects are bleeding into the test
+		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\DataDesign\\Tweet", $results);
+
+		// grab the result from the array and validate it
+		$pdoTweet = $results[0];
+		$this->assertEquals($pdoTweet->getTweetId(), $tweetId);
+		$this->assertEquals($pdoTweet->getTweetProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoTweet->getTweetContent(), $this->VALID_TWEETCONTENT);
+		//format the date too seconds since the beginning of time to avoid round off error
+		$this->assertEquals($pdoTweet->getTweetDate()->getTimestamp(), $this->VALID_TWEETDATE->getTimestamp());
+	}
+
+	/**
+	 * test grabbing all Tweets
+	 **/
+	public function testGetAllValidTweets() : void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tweet");
+
+		// create a new Tweet and insert to into mySQL
+		$tweetId = generateUuidV4();
+		$tweet = new Tweet($tweetId, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
+		$tweet->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Tweet::getAllTweets($this->getPDO());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tweet"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\DataDesign\\Tweet", $results);
+
+		// grab the result from the array and validate it
+		$pdoTweet = $results[0];
+		$this->assertEquals($pdoTweet->getTweetId(), $tweetId);
+		$this->assertEquals($pdoTweet->getTweetProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoTweet->getTweetContent(), $this->VALID_TWEETCONTENT);
+		//format the date too seconds since the beginning of time to avoid round off error
+		$this->assertEquals($pdoTweet->getTweetDate()->getTimestamp(), $this->VALID_TWEETDATE->getTimestamp());
+	}
+}
