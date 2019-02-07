c1517c6bcd1e9cd162e9107aa2f73c8109235fa3
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-07 13:48:08 -0700

    initial commmit to Profile unit test https://app.asana.com/0/1108517459409227/1108517459409235

---
+++ php/Classes/ProfileTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace abqtrails;
+
+use \abqtrails\Trail;
+
+require_once("autoload.php");
+
+require_once("../../vendor/autoload.php");
+
+/**
+ * Unit test for Profile Class
+ *
+ * @see \abqtrails\Profile
+ * @author Cassandra Romero <cromero278@cnm.edu>
+ */
+class ProfileTest extends AbqTrailsTest {
+
+}
