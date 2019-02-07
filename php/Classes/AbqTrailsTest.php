08e4a4dfb836385a972c8144efde08c03f89ff0d
Scott Wells <efwells101@gmail.com>
2019-02-07 09:54:41 -0700

    added unit test class file AbqTrailsTest; https://app.asana.com/0/1108517459409227/1108517459409231

---
+++ php/Classes/AbqTrailsTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace abqtrails;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\DbUnit\TestCaseTrait;
+use PHPUnit\DbUnit\DataSet\QueryDataSet;
+use PHPUnit\DbUnit\Database\Connection;
+use PHPUnit\DbUnit\Operation\{Composite, Factory, Operation};
+
+//grab the encrypted properties file
+require_once("/etc/apache2/capstone-mysql/Secret.php");
+
+require_once ("../../vendor/autoload.php");
+
+/**
+ * Abstract class contain universal and project specific mySQL parameters
+ *
+ * This class is design to lay the foundation of the unit tests per project. It loads all the database parameters about the
+ * project so that table specific tests can share the parameters in one place. To use it:
+ *
+ * 1. Rename the class from AbqTrailsTest to a project specific name (e.g. ProjectNameTest)
+ * 2. Rename the namespace to be the same as (1)
+ * 3. Modify AbqTrailsTest::getDataSet() to include all the tables in you project
+ * 4. Modify AbqTrailsTest::getConnection() to include the correct mySQL properties file
+ * 5. Have all table specific tests include this class
+ *
+ * *NOTE*: Tables must be added in the order they were created in step (2)
+ *
+ * @author Dylan McDonald <dmcdonald21@cnm.edu>
+ * @updated (for project) Scott Wells <swells19@cnm.edu>
+ **/
