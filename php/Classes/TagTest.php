5d86a939b88d7d1804177aaa09e857b32c94d700
Robert Dominguez <dominguez.1st@gmail.com>
2019-02-11 13:41:07 -0700

    Making progress on update. https://app.asana.com/0/1108517459409227/1108517459409233

--- php/Classes/TagTest.php
+++ php/Classes/TagTest.php
@@ -23,7 +23,7 @@ class TagTest extends  AbqTrailsTest {
 	 * name of the Tag
 	 * @var string $VALID_TAGNAME
 	 **/
-	protected $VALID_TAGNAME = "Gorgeous View";
+	protected $VALID_TAGNAME_1 = "Gorgeous View";
 
 	/**
 	 * name of the updated Tag
@@ -39,17 +39,30 @@ class TagTest extends  AbqTrailsTest {
 		// counts the number of rows and saves it for later
 		$numRows = $this->getConnection()->getRowCount("tag");
 		$tagId = generateUuidV4();
-		$tag = new Tag($tagId, $this->VALID_TAGNAME, $this->VALID_TAGNAME_2);
+		$tag = new Tag($tagId, $this->VALID_TAGNAME_1, $this->VALID_TAGNAME_2);
 		$tag->insert($this->getPDO());
 		// grab the data from mySQL and enforce the fields match our experience
 		$pdoTag = Tag::getTagByTagId($this->getPDO(),$tag->getTagId());
 		$this->assertEquals($numRows +1, $this->getConnection()->getRowCount("tag"));
 		$this->assertEquals($pdoTag->getTagId(), $tagId);
-		$this->assertEquals($pdoTag->getTagName(), $this->VALID_TAGNAME);
+		$this->assertEquals($pdoTag->getTagName(), $this->VALID_TAGNAME_1);
 		$this->assertEquals($pdoTag->getTagName2(), $this->VALID_TAGNAME_2 );
 	}
 	/**
 	 * test inserting a Tag, editing it, and then updating it
 	 **/
-
+	public function testUpdateValidTag() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("profile");
+		// create a new Tag and insert into mySQL
+		$tagId = generateUuidV4();
+		$tag = new Tag($tagId, $this->VALID_TAGNAME_1, $this->VALID_TAGNAME_2);
+		$tag-> insert($this->getPDO());
+		// edit the Tag and update it in mySQL
+		$tag->setTagName1($this->VALID_TAGNAME_1_2);
+		$tag->setTagName2($this->VALID_TAGNAME_2_2);
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoTag = Tag::getTagByTagId($this->getPDO(), $tag->getTagId());
+		$this->assert
+	}
 }
