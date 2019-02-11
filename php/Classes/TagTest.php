a92f0d625fc5d855e8cc6ed696ea2a35f612457e
Robert Dominguez <dominguez.1st@gmail.com>
2019-02-11 13:19:40 -0700

    Think I finished insert. https://app.asana.com/0/1108517459409227/1108517459409233

--- php/Classes/TagTest.php
+++ php/Classes/TagTest.php
@@ -29,7 +29,7 @@ class TagTest extends  AbqTrailsTest {
 	 * name of the updated Tag
 	 * @var string $VALID_TAGNAME
 	 **/
-	protected $VALID_TAGNAME2 = "Wheelchair Friendly";
+	protected $VALID_TAGNAME_2 = "Wheelchair Friendly";
 
 
 	/**
@@ -39,10 +39,17 @@ class TagTest extends  AbqTrailsTest {
 		// counts the number of rows and saves it for later
 		$numRows = $this->getConnection()->getRowCount("tag");
 		$tagId = generateUuidV4();
-		$tag = new Tag($tag, $this->VALID_TAGNAME);
+		$tag = new Tag($tagId, $this->VALID_TAGNAME, $this->VALID_TAGNAME_2);
 		$tag->insert($this->getPDO());
 		// grab the data from mySQL and enforce the fields match our experience
 		$pdoTag = Tag::getTagByTagId($this->getPDO(),$tag->getTagId());
-		$this->assertEquals(numRows +1)
+		$this->assertEquals($numRows +1, $this->getConnection()->getRowCount("tag"));
+		$this->assertEquals($pdoTag->getTagId(), $tagId);
+		$this->assertEquals($pdoTag->getTagName(), $this->VALID_TAGNAME);
+		$this->assertEquals($pdoTag->getTagName2(), $this->VALID_TAGNAME_2 );
 	}
+	/**
+	 * test inserting a Tag, editing it, and then updating it
+	 **/
+
 }
