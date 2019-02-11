5a0da030ba7506cece3058ecc9c1254981c28b79
Robert Dominguez <dominguez.1st@gmail.com>
2019-02-11 15:15:50 -0700

    Made the docblock. https://app.asana.com/0/1108517459409227/1108517459409236

---
+++ php/Classes/Tests/RatingTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace CapstoneTrails\AbqTrails;
+
+//use \abqtrails\Tag;
+
+//grab the class under scrutiny
+require_once(dirname(__DIR__, 1) . "/autoload.php");
+
+/**
+ * Full PHPUnit for the Rating class
+ *
+ * This is a complete PHPUnit Tests of the Rating class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see Rating
+ * @author Robert Dominguez
+ **/
