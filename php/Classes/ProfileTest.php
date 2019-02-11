56b1ea3bf1bba2c1de5695ec032eb2d1545306ae
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-11 13:34:10 -0700

    added get all profiles and typo fixes https://app.asana.com/0/1108517459409227/1108517459409230

--- php/Classes/ProfileTest.php
+++ php/Classes/ProfileTest.php
@@ -156,7 +156,7 @@ class ProfileTest extends AbqTrailsTest {
 /**
  * public function that gets profile by the username
  */
-	public function getProfileByProfileUsername() {
+	public function testGetProfileByProfileUsername() {
 		$numRows = $this->getConnection()->getRowCount("profile");
 		$profileId = generateUuidV4();
 		$profile = new \abqtrails\Profile($profileId, $this->VALID_PROFILE_ACTIVATION_TOKEN, $this->VALID_PROFILE_AVATAR_URL,
@@ -165,7 +165,7 @@ class ProfileTest extends AbqTrailsTest {
 		$profile->insert($this->getPDO());
 		$results = Profile:: getProfileByProfileUsername($this->getPDO(), $this->VALID_PROFILE_USERNAME);
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("profile"));
-		$this->assertContainsOnlyInstancesOf("\Abqtrails\Profile", $results);
+		$this->assertContainsOnlyInstancesOf("\\AbqTrails\\Profile", $results);
 		$pdoProfile = $results[0];
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("profile"));
 		$this->assertEquals($pdoProfile->getProfileId(), $profileId);
@@ -188,10 +188,10 @@ class ProfileTest extends AbqTrailsTest {
 	/*
  	* public function that gets profile by profile email
  	*/
-	public function getProfileByProfileEmail() {
+	public function testGetProfileByProfileEmail() {
 		$numRows = $this->getConnection()->getRowCount("profile");
 		$profileId = generateUuidV4();
-		$profile = new \abqtrails\Profile($profileId, $this->VALID_PROFILE_ACTIVATION_TOKEN, $this->VALID_PROFILE_AVATAR_URL,
+		$profile = new Profile($profileId, $this->VALID_PROFILE_ACTIVATION_TOKEN, $this->VALID_PROFILE_AVATAR_URL,
 			$this->VALID_PROFILE_EMAIL, $this->VALID_PROFILE_FIRST_NAME, $this->VALID_PROFILE_HASH, $this->VALID_PROFILE_LAST_NAME,
 			$this->VALID_PROFILE_USERNAME);
 		$profile->insert($this->getPDO());
@@ -212,5 +212,29 @@ class ProfileTest extends AbqTrailsTest {
 	public function testGetInvalidProfileByEmail() : void {
 		$profile = Profile::getProfileByProfileEmail($this->getPDO(), "invalid@email.co");
 		$this->assertNull($profile);
+	}
+	/**
+	 * public function that grabs all profiles
+	 */
+	public function testGetAllValidProfiles() : void {
+		$numRows = $this->getConnection()->getRowCount("profile");
+		$profileId = generateUuidV4();
+		$profile = new Profile($profileId, $this->VALID_PROFILE_ACTIVATION_TOKEN, $this->VALID_PROFILE_AVATAR_URL,
+			$this->VALID_PROFILE_EMAIL, $this->VALID_PROFILE_FIRST_NAME, $this->VALID_PROFILE_HASH, $this->VALID_PROFILE_LAST_NAME,
+			$this->VALID_PROFILE_USERNAME);
+		$profile->insert($this->getPDO());
+		$results = Profile::getAllProfiles($this->getPDO());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("profile"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("\\AbqTrails\\Profile", $results);
+		$pdoProfile = $results[0];
+		$this->assertEquals($pdoProfile->getProfileId(), $profileId);
+		$this->assertEquals($pdoProfile->getProfileActivationToken(), $this->VALID_PROFILE_ACTIVATION_TOKEN);
+		$this->assertEquals($pdoProfile->getProfileAvatarUrl(), $this->VALID_PROFILE_AVATAR_URL);
+		$this->assertEquals($pdoProfile->getProfileEmail(), $this->VALID_PROFILE_EMAIL);
+		$this->assertEquals($pdoProfile->getprofileFirstName(), $this->VALID_PROFILE_FIRST_NAME);
+		$this->assertEquals($pdoProfile->getProfileHash(), $this->VALID_PROFILE_HASH);
+		$this->assertEquals($pdoProfile->getProfileLastName(), $this->VALID_PROFILE_LAST_NAME);
+		$this->assertEquals($pdoProfile->getProfileUsername(), $this->VALID_PROFILE_USERNAME);
 	}
 }
