b1cc5a54e62a74ddb1604081fff3a8a2c95ab355
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-12 15:21:10 -0700

    added trail tag test https://app.asana.com/0/1108517459409227/1108517459409234

---
+++ php/Classes/Tests/TrailTagTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace CapstoneTrails\AbqTrails\Tests;
+
+use CapstoneTrails\AbqTrails\Classes\Profile;
+
+require_once(dirname(__DIR__, 1) . "/autoload.php");
+
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+
+class TrailTagTest extends  AbqTrailsTest {
+
+}
