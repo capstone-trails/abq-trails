19bceea6f66492a89079cf7c220cd6e07bf08435
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-14 10:34:34 -0700

    trail tag test protected statements https://app.asana.com/0/1108517459409227/1108517459409234

--- php/Classes/Tests/TrailTagTest.php
+++ php/Classes/Tests/TrailTagTest.php
@@ -1,11 +1,39 @@
 <?php
 namespace CapstoneTrails\AbqTrails\Tests;
 
-use CapstoneTrails\AbqTrails\Classes\TrailTag;
+use CapstoneTrails\AbqTrails\Profile;
+use CapstoneTrails\AbqTrails\TrailTag;
+use CapstoneTrails\AbqTrails\Tag;
 
 require_once(dirname(__DIR__, 1) . "/autoload.php");
 
+/**
+ * @see TrailTag
+ *
+*/
 
 class TrailTagTest extends  AbqTrailsTest {
+	/**
+	 * Tag from tag/trail tag relationship, primary key
+	 * @var Tag $tag
+	 */
+	protected $tag = null;
+	/**
+	 * Trail from trail/trail tag relationship, primary key
+	 */
+	protected $trail = null;
+	/**
+	 * Profile from profile/trail tag relationship, foreign key
+	 * @var Profile $profile
+	 */
+	protected $profile = null;
 
+	protected $VALID_HASH;
+
+	protected $VALID_ACTIVATION;
+	/**
+	 * create dependent objects before running each test
+	 *
+	 * @throws \Exception
+	 */
 }
