c9a73bdfb729cbd90e3a1279eed3449b593fafbb
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-14 13:21:47 -0700

    trail tag test insert statements https://app.asana.com/0/1108517459409227/1108517459409234

--- php/Classes/Tests/TrailTagTest.php
+++ php/Classes/Tests/TrailTagTest.php
@@ -2,6 +2,7 @@
 namespace CapstoneTrails\AbqTrails\Tests;
 
 use CapstoneTrails\AbqTrails\Profile;
+use CapstoneTrails\AbqTrails\Trail;
 use CapstoneTrails\AbqTrails\TrailTag;
 use CapstoneTrails\AbqTrails\Tag;
 
@@ -36,4 +37,18 @@ class TrailTagTest extends  AbqTrailsTest {
 	 *
 	 * @throws \Exception
 	 */
-}
+	public final function setUp(): void {
+		parent::setUp();
+		$password = "heythere123";
+		$this->VALID_HASH = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 384]);
+		$this->VALID_ACTIVATION = bin2hex(random_bytes(16));
+
+		//create and insert a profile
+		$this->profile = new Profile(generateUuidV4(), $this->VALID_ACTIVATION, "www.blahblah.com/12222", "myname@man.com", "Matt", $this->VALID_HASH, "Damon", "mattDamon");
+		$this->profile->insert($this->getPDO());
+
+		//create and insert trail from trail tag
+		$this->trail = new Trail(generateUuidV4(), "www.faketrail.com/photo", "This trail is a fine trail", "1234", "35.0792", "5.2", "106.4847", "`1254`", "Copper Canyon");
+		$this->trail->insert($this->getPDO());d
+		}
+	}
