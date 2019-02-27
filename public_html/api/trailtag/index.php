ed97f4ec1321a3a4aaabf0408cc32e66b6e75e0e
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-27 16:56:11 -0700

    added trail tag api index

---
+++ public_html/api/trailtag/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
+
+use CapstoneTrails\AbqTrails\ {
+	TrailTag
+};
+
