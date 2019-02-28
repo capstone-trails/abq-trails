7ec83efacc5867dd0771b79c58f44ce4a53d691f
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-28 10:52:01 -0700

    secrets  https://app.asana.com/0/1108517459409227/1111859062528127

--- public_html/api/trailtag/index.php
+++ public_html/api/trailtag/index.php
@@ -2,7 +2,7 @@
 require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/Classes/autoload.php";
 require_once(dirname(__DIR__, 3) . "/php/lib/xsrf.php");
-require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 
 use CapstoneTrails\AbqTrails\ {
