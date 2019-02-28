90d0dbb4198faa69029f0d9b3bfa8b2299a73dbb
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-28 10:25:35 -0700

    sign out api  https://app.asana.com/0/1108517459409227/1111859062528127

---
+++ public_html/api/sign-out/index.php
@@ -0,0 +1,49 @@
+<?php
+require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
+require_once(dirname(__DIR__, 3) . "/php/Classes/autoload.php");
+require_once(dirname(__DIR__, 3) . "/php/lib/xsrf.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+/**
+ *
+ * api for signing out
+ *
+ * @author Cassandra Romero
+ *
+ *
+ */
+//verify the user has xsrf
+if(session_status() !== PHP_SESSION_ACTIVE){
+	session_start();
+}
+//prepare default error message
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+	//grab the mySQL connection
+	//grab the mysql connection
+	$secrets = new \Secrets("/etc/apache2/capstone-mysql/cohort23/trails.ini");
+	$pdo = $secrets->getPdoObject();	//determine which HTTP method was used
+
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	if($method === "GET") {
+		$_SESSION = [];
+		$reply->message = "You are signed out";
+	}
+	else {
+		throw (new \InvalidArgumentException("Invalid HTTP method request"));
+	}
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+}
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+//encode and return reply to front end caller
+echo json_encode($reply);
