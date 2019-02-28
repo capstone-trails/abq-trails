9953e1b7f6f469b789194ebab18389dc57e35e88
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-28 14:09:12 -0700

    Sign out edits https://app.asana.com/0/1108517459409227/1111859062528127

--- public_html/api/sign-up/index.php
+++ public_html/api/sign-up/index.php
@@ -1,10 +1,9 @@
 <?php
 require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
-require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/Classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
-require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
 
 use CapstoneTrails\AbqTrails\Profile;
 /**
