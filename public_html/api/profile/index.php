9114fb1468fd6ffd875cea42e562bd129c37b767
Scott Wells <efwells101@gmail.com>
2019-02-28 09:19:38 -0700

    modify/fix errors on profile api; https://app.asana.com/0/1108517459409227/1111859062528129

--- public_html/api/profile/index.php
+++ public_html/api/profile/index.php
@@ -9,6 +9,8 @@ require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 use CapstoneTrails\AbqTrails\Profile;
 
 /**
+ * api for the Profile class
+ *
  * @author Scott Wells <swells19@cnm.edu>
  **/
 
@@ -24,36 +26,35 @@ $reply->data = null;
 
 try {
 	//grab the mysql connection
-	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cohort23/trails.ini");
+	$secrets = new \Secrets("/etc/apache2/capstone-mysql/cohort23/trails.ini");
+	$pdo = $secrets->getPdoObject();
 
 	//determine which HTTP method was used
-	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 
 	//sanitize input
 	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$profileEmail = filter_input(INPUT_GET, "profileEmail", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
 	//make sure id is valid for methods that require it
 	if(($method === "PUT") && (empty($id) === true)) {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
 	}
 
-	//verify the user has a XSRF token
-	verifyXsrf();
+	if($method === "PUT") {
+		//verify the user has a XSRF token
+		verifyXsrf();
 
-	/**
-	 * Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input")
-	 * to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string.
-	 * The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read
-	 * from the front end request which is, in this case, a JSON package.
-	 **/
-	$requestContent = file_get_contents("php://input");
+		/**
+		 * Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input")
+		 * to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string.
+		 * The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read
+		 * from the front end request which is, in this case, a JSON package.
+		 **/
+		$requestContent = file_get_contents("php://input");
 
-	//this line decodes the JSON package and stores that result in $requestObject
-	$requestObject = json_decode($requestContent);
+		//this line decodes the JSON package and stores that result in $requestObject
+		$requestObject = json_decode($requestContent);
 
-	//perform the actual PUT
-	if($method === PUT) {
 		//retrieve the profile to update
 		$profile = Profile::getProfileByProfileId($pdo, $id);
 		if($profile === null) {
@@ -61,7 +62,7 @@ try {
 		}
 
 		//enforce the user is signed in and can only update their own profile
-		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId()->toString() !== $profile->getProfileEmail()->toString()) {
+		if(empty($_SESSION["profile"]) === true) {
 			throw(new \InvalidArgumentException("You have to be logged in to edit this profile", 403));
 		}
 
@@ -87,5 +88,5 @@ try {
 header("Content-type: application/json");
 echo json_encode($reply);
 
-//JSON encodes the $reply object and sends it back to the front end.
+//	JSON encodes the $reply object and sends it back to the front end.
 
