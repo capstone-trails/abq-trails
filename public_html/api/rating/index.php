60d013189b2a3b6d7208cfed1007a570d41b0302
Robert Dominguez <dominguez.1st@gmail.com>
2019-02-28 10:47:53 -0700

    Made GET. https://app.asana.com/0/1108517459409227/1111859062528131

--- public_html/api/rating/index.php
+++ public_html/api/rating/index.php
@@ -35,4 +35,67 @@ $reply->data = null;
 
 try {
 	// grab the mySQl connection
+	$secrets = new \Secrets("/etc/apache2/capstone-mysql/cohort23/rating.ini");
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cohort23/rating.ini");
+
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//sanitize the search parameters
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+	$ratingProfileId = $id = filter_input(INPUT_GET, "ratingProfileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$ratingTrailId = $id = filter_input(INPUT_GET, "ratingTrailId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+	if(($method === "DELETE") && (empty($id) === true)){
+		throw(new \InvalidArgumentException("Id cannot be empty", 405));
+	}
+
+	//process actual GET and POST methods
+	if($method === "GET") {
+		//set XSRF token
+		setXsrfCookie();
+
+		//get a rating by id and update reply
+		if(empty($id) === false) {
+			$rating = Rating::getRatingByRatingProfileIdAndRatingTrailId($pdo, $id);
+		} else if(empty($ratingProfileId) === false) {
+			$reply->data = Rating::getRatingByRatingProfileId($pdo, $ratingProfileId)->toArray();
+		} else if(empty($ratingTrailId) === false) {
+			$reply->data = Rating::getRatingByRatingTrailId($pdo, $ratingTrailId)->toArray();
+		}
+	} else if($method === "POST") {
+		// enforce the user has a XSRF token
+		verifyXsrf();
+		//Retrieve the Json package and store in $requestContent
+		$requestContent = file_get_contents("php://input");
+		// Decode the JSON package and stores that result in $requestObject
+		$requestObject = json_decode($requestContent);
+
+		if(empty($requestObject->trailTagTagId) === true) {
+			throw (new \InvalidArgumentException("No tag linked to the trail tag", 405));
+		}
+		if(empty($requestObject->trailTagTrailId) === true) {
+			throw (new \InvalidArgumentException("No trail linked to the trail tag", 405));
+		}
+		if(empty($requestObject->trailTagProfileId) === true) {
+			throw (new \InvalidArgumentException("No profile linked to the trail tag", 405));
+		}
+		//enforce the user is signed in to tag the trail
+		if(empty($_SESSION ["profile"]) === true) {
+			throw(new \InvalidArgumentException("You must be logged in to tag a trail", 403));
+		}
+
+		$trailTag = new TrailTag($requestObject->trailTagTagId, $requestObject->trailTagTrailId, $_SESSION["profile"]->getProfileId);
+		$trailTag->insert($pdo);
+
+		//tag reply
+		$reply->message = "Tag added to trail";
+
+
+	}
+}catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
 }
+
+header("Content-Type: application/json");
+echo json_encode($reply);
