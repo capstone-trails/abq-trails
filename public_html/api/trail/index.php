4fa5c63de8a3b8dde88b4fbf84fb596627eab790
Scott Wells <efwells101@gmail.com>
2019-02-28 08:36:48 -0700

    write first part of try block in trail api ; https://app.asana.com/0/1108517459409227/1111859062528137

--- public_html/api/trail/index.php
+++ public_html/api/trail/index.php
@@ -8,3 +8,40 @@ require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 use CapstoneTrails\AbqTrails\{Trail, Profile};
 
+/**
+ * api for Trail class
+ *
+ * @author Scott Wells <swells19@cnm.edu>
+ **/
+
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//grab the mysql connection
+	$secrets = new \Secrets("/etc/apache2/capstone-mysql/cohort23/trails.ini");
+	$pdo = $secrets->getPdoObject();
+
+	//determine which GTTP method was used
+	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
+
+	//sanitize input
+	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$trailAvatarUrl = filter_input(INPUT_GET, "trailAvatarUrl", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$trailDescription = filter_input(INPUT_GET, "trailDescription", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$trailHigh = filter_input(INPUT_GET, "trailHigh", FILTER_SANITIZE_NUMBER_INT);
+	$trailLatitude = filter_input(INPUT_GET, "trailLatitude", FILTER_SANITIZE_NUMBER_FLOAT);
+	$trailLength = filter_input(INPUT_GET, "trailLength", FILTER_SANITIZE_NUMBER_FLOAT);
+	$trailLongitude = filter_input(INPUT_GET, "trailLongitude", FILTER_SANITIZE_NUMBER_FLOAT);
+	$trailLow = filter_input(INPUT_GET, "trailLow", FILTER_SANITIZE_NUMBER_INT);
+	$trailName = filter_input(INPUT_GET, "trailName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+
+}
