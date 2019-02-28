f1385bcd299ba2fe5d6cd50dd85d44ea607734f6
Robert Dominguez <dominguez.1st@gmail.com>
2019-02-28 09:16:00 -0700

    Working on rating api, wrote some more code. https://app.asana.com/0/1108517459409227/1111859062528131

--- public_html/api/rating/index.php
+++ public_html/api/rating/index.php
@@ -7,5 +7,18 @@ require_once(dirname(__DIR__, 3) . "/php/lib/xsrf.php");
 require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
 require_once(dirname(__DIR__, 3) . "/php/lib/jwt.php");
 
-use CapstoneTrails\AbqTrails\Profile;
-use CapstoneTrails\AbqTrails\Trail;
+use CapstoneTrails\AbqTrails\{
+	Profile,
+	Trail
+};
+
+
+/**
+ * api for trail tag class
+ *
+ * @author Robert Dominguez <rdominguez45@cnm.edu>
+ **/
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
