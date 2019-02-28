eb771fafd0740f54fd7e66df4aa00655a5802c8b
Cassandra Romero <romero.cassandra1@gmail.com>
2019-02-27 19:04:56 -0700

    sanitize input parameters trail tag api https://app.asana.com/0/1108517459409227/1111859062528127

--- public_html/api/trailtag/index.php
+++ public_html/api/trailtag/index.php
@@ -21,3 +21,16 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
+//grab the mySQL connection
+	try {
+		//grab the mySQL connection
+		$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cohort23/trails.ini");
+
+		$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+		//sanitize the search parameters
+		$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+		$trailTagTagId = $id = filter_input(INPUT_GET, "trailTagTagId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		$trailTagTrailId = $id = filter_input(INPUT_GET, "trailTagTrailId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		$trailTagProfileId = $id = filter_input(INPUT_GET, "trailTagProfileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+	}
