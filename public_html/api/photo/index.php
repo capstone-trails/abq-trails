d8d1e6573a043331ac39032571f24552ebda61de
Scott Wells <efwells101@gmail.com>
2019-02-28 10:51:34 -0700

    update filepath in headers; https://app.asana.com/0/1108517459409227/1111859062528123

--- public_html/api/photo/index.php
+++ public_html/api/photo/index.php
@@ -3,8 +3,10 @@ require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
 require_once(dirname(__DIR__, 3) . "/php/Classes/autoload.php");
 require_once(dirname(__DIR__, 3) . "/php/lib/xsrf.php");
 require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
+
+//include the jwt for Cloudinary
 require_once(dirname(__DIR__, 3) . "/php/lib/jwt.php");
-require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 use CapstoneTrails\AbqTrails\{Photo, Profile};
 
