af877e34fc00bb013712930abdbde6d9f803fb39
Scott Wells <efwells101@gmail.com>
2019-03-06 10:48:08 -0700

    add file paths, fix typo ;https://app.asana.com/0/1108517459409227/1111859062528131

--- public_html/api/photo/index.php
+++ public_html/api/photo/index.php
@@ -4,11 +4,8 @@ require_once(dirname(__DIR__, 3) . "/php/Classes/autoload.php");
 require_once(dirname(__DIR__, 3) . "/php/lib/xsrf.php");
 require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
 require_once(dirname(__DIR__, 3) . "/php/lib/jwt.php");
-
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
 
-//include the jwt for Cloudinary
-require_once(dirname(__DIR__, 3) . "/php/lib/jwt.php");
 
 use CapstoneTrails\AbqTrails\{Photo, Profile};
 
