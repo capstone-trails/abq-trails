7ea3471c7e1a993d1514dac667fb4a0e6ba88c1a
Scott Wells <efwells101@gmail.com>
2019-03-06 09:43:04 -0700

    move curly brace back; https://app.asana.com/0/1108517459409227/1111859062528129

--- public_html/api/profile/index.php
+++ public_html/api/profile/index.php
@@ -50,80 +50,81 @@ try {
 		if(empty($profileEmail) === false) {
 			$reply->data = Profile::getProfileByProfileEmail($pdo, $profileEmail);
 		}
-
-		//make sure id is valid for methods that require it
-		if(($method === "PUT") && (empty($id) === true)) {
-			throw(new InvalidArgumentException("id cannot be empty or negative", 405));
-
-		} else if($method === "PUT") {
-			//verify the user has a XSRF token and validate Jwt Header;
-			verifyXsrf();
-
-			//enforce the user is signed in and can only update their own profile
-			if(empty($_SESSION["profile"]) === true) {
-				throw(new \InvalidArgumentException("You have to be logged in to edit this profile", 403));
-			}
-
-			validateJwtHeader();
-
-
-			/**
-			 * Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input")
-			 * to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string.
-			 * The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read
-			 * from the front end request which is, in this case, a JSON package.
-			 **/
-			$requestContent = file_get_contents("php://input");
-
-			//this line decodes the JSON package and stores that result in $requestObject
-			$requestObject = json_decode($requestContent);
-
-			//retrieve the profile to update
-			$profile = Profile::getProfileByProfileId($pdo, $id);
-			if($profile === null) {
-				throw(new RuntimeException("Profile does not exist", 404));
-			}
-
-			//if profile avatar url is empty, use the one already in the data base
-			if(empty($requestObject->profileAvatarUrl) === true) {
-				$requestObject->profileAvatarUrl = $profile->getProfileAvatarUrl();
-			}
-
-			//if profile email is empty, use the one already in the data base
-			if(empty($requestObject->profileEmail) === true) {
-				$requestObject->profileEmail = $profile->getProfileEmail();
-			}
-
-			//if profile first name is empty, use the one already in the data base
-			if(empty($requestObject->profileFirstName) === true) {
-				$requestObject->profileFirstName = $profile->getProfileFirstName();
-			}
-
-			//if profile last name is empty, use the one already in the data base
-			if(empty($requestObject->profileLastName) === true) {
-				$requestObject->profileLastName = $profile->getProfileLastName();
-			}
-
-			//if profile username is empty, use the one already in the data base
-			if(empty($requestObject->profileUsername) === true) {
-				$requestObject->profileUsername = $profile->getProfileUsername();
-			}
-
-			//update all attributes
-			$profile->setProfileAvatarUrl($requestObject->profileAvatarUrl);
-			$profile->setProfileEmail($requestObject->profileEmail);
-			$profile->setProfileFirstName($requestObject->profileFirstName);
-			$profile->setProfileLastName($requestObject->profileLastName);
-			$profile->setProfileUsername($requestObject->profileUsername);
-			$profile->update($pdo);
-
-			//update reply message
-			$reply->message = "Profile updated!";
-
-		} else {
-			throw(new \InvalidArgumentException("Invalid HTTP request", 400));
-		}
 	}
+
+	//make sure id is valid for methods that require it
+	if(($method === "PUT") && (empty($id) === true)) {
+		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+
+	} else if($method === "PUT") {
+		//verify the user has a XSRF token and validate Jwt Header;
+		verifyXsrf();
+
+		//enforce the user is signed in and can only update their own profile
+		if(empty($_SESSION["profile"]) === true) {
+			throw(new \InvalidArgumentException("You have to be logged in to edit this profile", 403));
+		}
+
+		validateJwtHeader();
+
+
+		/**
+		 * Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input")
+		 * to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string.
+		 * The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read
+		 * from the front end request which is, in this case, a JSON package.
+		 **/
+		$requestContent = file_get_contents("php://input");
+
+		//this line decodes the JSON package and stores that result in $requestObject
+		$requestObject = json_decode($requestContent);
+
+		//retrieve the profile to update
+		$profile = Profile::getProfileByProfileId($pdo, $id);
+		if($profile === null) {
+			throw(new RuntimeException("Profile does not exist", 404));
+		}
+
+		//if profile avatar url is empty, use the one already in the data base
+		if(empty($requestObject->profileAvatarUrl) === true) {
+			$requestObject->profileAvatarUrl = $profile->getProfileAvatarUrl();
+		}
+
+		//if profile email is empty, use the one already in the data base
+		if(empty($requestObject->profileEmail) === true) {
+			$requestObject->profileEmail = $profile->getProfileEmail();
+		}
+
+		//if profile first name is empty, use the one already in the data base
+		if(empty($requestObject->profileFirstName) === true) {
+			$requestObject->profileFirstName = $profile->getProfileFirstName();
+		}
+
+		//if profile last name is empty, use the one already in the data base
+		if(empty($requestObject->profileLastName) === true) {
+			$requestObject->profileLastName = $profile->getProfileLastName();
+		}
+
+		//if profile username is empty, use the one already in the data base
+		if(empty($requestObject->profileUsername) === true) {
+			$requestObject->profileUsername = $profile->getProfileUsername();
+		}
+
+		//update all attributes
+		$profile->setProfileAvatarUrl($requestObject->profileAvatarUrl);
+		$profile->setProfileEmail($requestObject->profileEmail);
+		$profile->setProfileFirstName($requestObject->profileFirstName);
+		$profile->setProfileLastName($requestObject->profileLastName);
+		$profile->setProfileUsername($requestObject->profileUsername);
+		$profile->update($pdo);
+
+		//update reply message
+		$reply->message = "Profile updated!";
+
+	} else {
+		throw(new \InvalidArgumentException("Invalid HTTP request", 400));
+	}
+
 
 
 //update the $reply->status $reply->message
