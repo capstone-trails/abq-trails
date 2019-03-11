ae3bcdfc57e4f6e0725138b8f50fb8124184319b
Ronald Luna <ronaldluna1@gmail.com>
2019-03-11 14:42:30 -0600

    profile-modal.php sizing https://app.asana.com/0/1108517459409227/1108517459409235

--- static-ui/profile-modal.php
+++ static-ui/profile-modal.php
@@ -1,76 +1,41 @@
 <?php require_once ("head-utils.php");?>
 
-
+<div class="boxed">.
 <main>
-	<!-- Button trigger modal -->
-	<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#profileModal">
-		Launch demo modal
-	</button>
+	<table border="1"
+	<table class="container-fluid">
+		<img src="https://via.placeholder.com/85" class="rounded mx-auto d-block" alt="...">
+		<h2 class="text-center">Profile Name</h2>
 
+		<!-- Equal width cols, same on all screen sizes -->
 
-	<!-- Modal -->
-
-
-	<div class="modal fade" id="profileModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h5 class="modal-title" id="exampleModalLabel">My Profile</h5>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<p align="left">FirstLastName</p>
 				</div>
-				<div class="modal-body">
-					<div class="container-fluid">
-						<img src="https://via.placeholder.com/80" class="rounded mx-auto d-block" alt="...">
-						<h2 class="text-center">Profile Name</h2>
-
-						<!-- Equal width cols, same on all screen sizes -->
-						<div class="container">
-							<div class="row">
-								<div class="col">
-									<p align="left">FirstLastName</p>
-								</div>
-								<div class="col">
-								</div>
-								<div class="col">
-									<p align="right">Email@email.com</p>
-								</div>
-							</div>
-						</div>
-
-
-						<h2 align="center">My Photos</h2>
-						<div class="container">
-							<div class="row">
-								<div class="col">
-									<img src="https://via.placeholder.com/80" class="rounded mx-auto d-block" alt="...">
-								</div>
-								<div class="col order-12">
-									<img src="https://via.placeholder.com/80" class="rounded mx-auto d-block" alt="...">
-								</div>
-								<div class="col order-1">
-									<img src="https://via.placeholder.com/80" class="rounded mx-auto d-block" alt="...">
-								</div>
-
-								<div class="col">
-									<img src="https://via.placeholder.com/80" class="rounded mx-auto d-block" alt="...">
-								</div>
-								<div class="col">
-									<img src="https://via.placeholder.com/80" class="rounded mx-auto d-block" alt="...">
-								</div>
-						</div>
-
-					</div>
+				<div class="col">
 				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-primary">Edit Profile</button>
-					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-
+				<div class="col">
+					<p align="right">Email@email.com</p>
 				</div>
 			</div>
 		</div>
+
+
+		<h2 align="center">My Photos</h2>
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<img src="https://via.placeholder.com/95" class="rounded mx-auto d-block" alt="...">
+				</div>
+				<div class="col order-12">
+					<img src="https://via.placeholder.com/95" class="rounded mx-auto d-block" alt="...">
+				</div>
+				<div class="col order-1">
+					<img src="https://via.placeholder.com/95" class="rounded mx-auto d-block" alt="...">
+				</div>
+			</div>
+		</div>
+	</table>
 	</div>
-</main>
-
-
