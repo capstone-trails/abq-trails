42fcc7e7ffdc07c129b157f44a20f0b7b2ed785f
Scott Wells <efwells101@gmail.com>
2019-03-11 13:53:01 -0600

    made a working search bar for trail list and map view

--- static-ui/trail-list.php
+++ static-ui/trail-list.php
@@ -1,13 +1,15 @@
 <?php require_once ("head-utils.php"); ?>
 
-<?php //require_once ("navbar.php"); ?>
+<?php require_once ("navbar.php"); ?>
 
 <main class="trail-list">
-	<section class="container mt-3">
-		<form class="form-inline ">
-				<input class="form-control d-inline-block" type="search" placeholder="Search" aria-label="Search">
-				<button class="btn" type="submit"><i class="fas fa-search"></i></button>
-
-		</form>
+	<section class="container-fluid justify-content-center mx-0">
+		<form action="" autocomplete="off" class="navbar-form" method="post" accept-charset="utf-8">
+			<div class="input-group col-lg-12">
+				<input name="searchtext" style="width:100%" value="" class="form-control" type="text">
+				<span class="input-group-btn">
+					<button class="btn" type="submit"><i class="fas fa-search"></i></button>
+    			</span>
+			</div>
 	</section>
 </main>
