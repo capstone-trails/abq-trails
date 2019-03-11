448a1a92e4a06e4f9c80f2bc790298e87bcf2d6d
Scott Wells <efwells101@gmail.com>
2019-03-11 13:27:54 -0600

    added font awesome cdn link to head-utils

--- static-ui/head-utils.php
+++ static-ui/head-utils.php
@@ -11,6 +11,9 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
+	<!-- Font Awesome Icons -->
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+
 	<!-- Stylesheet -->
 	<script src="css/styles.css"></script>
 
---
+++ static-ui/trail-list.php
@@ -0,0 +1,13 @@
+<?php require_once ("head-utils.php"); ?>
+
+<?php //require_once ("navbar.php"); ?>
+
+<main class="trail-list">
+	<section class="container mt-3">
+		<form class="form-inline ">
+				<input class="form-control d-inline-block" type="search" placeholder="Search" aria-label="Search">
+				<button class="btn" type="submit"><i class="fas fa-search"></i></button>
+
+		</form>
+	</section>
+</main>
