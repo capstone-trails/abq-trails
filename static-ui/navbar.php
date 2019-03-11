576b229050b3d2aee905a3e4c12e14b3e3dfa7d3
Scott Wells <efwells101@gmail.com>
2019-03-11 15:25:01 -0600

    added name to the navbar

--- static-ui/navbar.php
+++ static-ui/navbar.php
@@ -4,7 +4,7 @@
 	<div class="row">
 		<div class="col">
 			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-					<a class="navbar-brand" href="#"><i class="fas fa-mountain"></i></a>
+					<a class="navbar-brand" href="#"><i class="fas fa-mountain"></i> ABQ TRAILS</a>
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 						<span class="navbar-toggler-icon"></span>
 					</button>
