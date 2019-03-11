7a4317433b7319634022e7842f9b301441258571
Ronald Luna <ronaldluna1@gmail.com>
2019-03-11 13:57:40 -0600

    profile-modal.php done https://app.asana.com/0/1108517459409227/1108517459409235

--- static-ui/profile-modal.php
+++ static-ui/profile-modal.php
@@ -3,45 +3,39 @@
 
 <main>
 	<div class="container-fluid">
-		<img src="https://via.placeholder.com/250" class="rounded mx-auto d-block" alt="...">
+		<img src="https://via.placeholder.com/80" class="rounded mx-auto d-block" alt="...">
 		<h1 class="text-center">Profile Name</h1>
 
 		<!-- Equal width cols, same on all screen sizes -->
 		<div class="container">
 			<div class="row">
 				<div class="col">
-					<h2 align="left">FirstLastName</h2>
+					<p align="left">FirstLastName</p>
 				</div>
 				<div class="col">
 				</div>
 				<div class="col">
-					<h2 align="right">Email@email.com</h2>
+					<p align="right">Email@email.com</p>
 				</div>
 			</div>
 		</div>
 
 
 		<h1 align="center">My Photos</h1>
-		<div class="card-deck">
-			<div class="card">
-				<img class="card-img-top" src="https://via.placeholder.com/100" alt="Card image cap">
-				<div class="card-body">
-
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<img src="https://via.placeholder.com/80" class="rounded mx-auto d-block" alt="...">
 				</div>
-			</div>
-			<div class="card">
-				<img class="card-img-top" src="https://via.placeholder.com/100" alt="Card image cap">
-				<div class="card-body">
-
+				<div class="col order-12">
+					<img src="https://via.placeholder.com/80" class="rounded mx-auto d-block" alt="...">
 				</div>
-			</div>
-			<div class="card">
-				<img class="card-img-top" src="https://via.placeholder.com/100" alt="Card image cap">
-				<div class="card-body">
-
+				<div class="col order-1">
+					<img src="https://via.placeholder.com/80" class="rounded mx-auto d-block" alt="...">
 				</div>
 			</div>
 		</div>
+
 
 	</div>
 </main>
