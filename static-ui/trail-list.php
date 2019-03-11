2b39522b2f977e5c23c31abbc8063a2bf3bae9e5
Scott Wells <efwells101@gmail.com>
2019-03-11 14:48:01 -0600

    finished trail list component

--- static-ui/trail-list.php
+++ static-ui/trail-list.php
@@ -3,7 +3,7 @@
 <?php require_once ("navbar.php"); ?>
 
 <main class="trail-list">
-	<section class="container-fluid justify-content-center mx-0">
+	<div class="container-fluid justify-content-center mx-auto my-3 p-0">
 		<form action="" autocomplete="off" class="navbar-form" method="post" accept-charset="utf-8">
 			<div class="input-group col-lg-12">
 				<input name="searchtext" style="width:100%" value="" class="form-control" type="text">
@@ -11,5 +11,80 @@
 					<button class="btn" type="submit"><i class="fas fa-search"></i></button>
     			</span>
 			</div>
-	</section>
+	</div>
+	<div class="container-fluid mb-2">
+		<div class="card">
+			<img src="https://via.placeholder.com/300x100" class="card-img-top" alt="...">
+			<div class="card-body">
+				<h5 class="card-title">Some Trail</h5>
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<div class="row">
+					<div class="col text-left">
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+				</div>
+				</div>
+				<div class="row">
+					<div class="col text-left">
+						<a href="#" class="badge badge-secondary">paved</a>
+						<a href="#" class="badge badge-secondary">camping</a>
+						<a href="#" class="badge badge-secondary">dog-friendly</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid mb-2">
+		<div class="card">
+			<img src="https://via.placeholder.com/300x100" class="card-img-top" alt="...">
+			<div class="card-body">
+				<h5 class="card-title">Some Trail</h5>
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<div class="row">
+					<div class="col text-left">
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col text-left">
+						<a href="#" class="badge badge-secondary">paved</a>
+						<a href="#" class="badge badge-secondary">camping</a>
+						<a href="#" class="badge badge-secondary">dog-friendly</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid mb-2">
+		<div class="card">
+			<img src="https://via.placeholder.com/300x100" class="card-img-top" alt="...">
+			<div class="card-body">
+				<h5 class="card-title">Some Trail</h5>
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<div class="row">
+					<div class="col text-left">
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col text-left">
+						<a href="#" class="badge badge-secondary">paved</a>
+						<a href="#" class="badge badge-secondary">camping</a>
+						<a href="#" class="badge badge-secondary">dog-friendly</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </main>
