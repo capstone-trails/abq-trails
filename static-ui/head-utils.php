e03a30b41e7dc3befb681bcc4e43dce0448562ec
Scott Wells <efwells101@gmail.com>
2019-03-11 11:58:43 -0600

    create new styles directory

--- static-ui/head-utils.php
+++ static-ui/head-utils.php
@@ -12,7 +12,7 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
 	<!-- Stylesheet -->
-	<script src="styles.css"></script>
+	<script src="css/styles.css"></script>
 
 	<title>ABQ Trails</title>
 
