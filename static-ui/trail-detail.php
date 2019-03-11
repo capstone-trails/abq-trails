9e8d4ecc056caadb49670898c19ce2fff05ea291
Scott Wells <efwells101@gmail.com>
2019-03-11 11:33:44 -0600

    align items in the containers; first half of the page

--- static-ui/trail-detail.php
+++ static-ui/trail-detail.php
@@ -3,13 +3,15 @@
 <?php //require_once ("navbar.php"); ?>
 
 <main class="trail-detail">
-	<section class="container mt-top">
-		<div class="container text-center">
-			<img src="https://via.placeholder.com/50" alt=""/>
+	<section class="container mt-5">
+		<div class="container text-center mb-3">
+			<img src="https://via.placeholder.com/100" alt=""/>
 			<h1>Some Trail</h1>
 		</div>
-		<div class="container">
-			<img src="https://via.placeholder.com/150"/>
+		<div class="container text-center">
+			<img src="https://via.placeholder.com/300x150"/>
+		</div>
+		<div class="container text-left mt-3 mb-5">
 			<p>Pastrami swine filet mignon pork porchetta rump pork loin. Bacon frankfurter pancetta ham hock alcatra capicola short loin rump bresaola meatball salami sausage tri-tip pork chop.</p>
 		</div>
 		<div class="container">
