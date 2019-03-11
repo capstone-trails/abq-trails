fe648d3ea9b1562e1a3f5c4de39819d2e2b9bc0c
Scott Wells <efwells101@gmail.com>
2019-03-11 11:57:32 -0600

    finish trail detail component

--- static-ui/trail-detail.php
+++ static-ui/trail-detail.php
@@ -3,36 +3,37 @@
 <?php //require_once ("navbar.php"); ?>
 
 <main class="trail-detail">
-	<section class="container mt-5">
+	<section class="container mt-3">
 		<div class="container text-center mb-3">
-			<img src="https://via.placeholder.com/100" alt=""/>
+			<img src="https://via.placeholder.com/75" alt=""/>
 			<h1>Some Trail</h1>
 		</div>
 		<div class="container text-center">
 			<img src="https://via.placeholder.com/300x150"/>
 		</div>
-		<div class="container text-left mt-3 mb-5">
+		<div class="container text-left my-3">
 			<p>Pastrami swine filet mignon pork porchetta rump pork loin. Bacon frankfurter pancetta ham hock alcatra capicola short loin rump bresaola meatball salami sausage tri-tip pork chop.</p>
 		</div>
 		<div class="container">
 			<div class="row">
-				<div class="col">
-					<img src="https://via.placeholder.com/50" alt=""/>
-					<img src="https://via.placeholder.com/50" alt=""/>
-					<img src="https://via.placeholder.com/50" alt=""/>
+				<div class="col my-3 d-flex justify-content-center">
+					<img class="px-3" src="https://via.placeholder.com/50" alt=""/>
+					<img class="px-3" src="https://via.placeholder.com/50" alt=""/>
+					<img class="px-3" src="https://via.placeholder.com/50" alt=""/>
 				</div>
 			</div>
 		</div>
 		<div class="container">
-			<p>Info at a glance:</p>
+			<p class="text-center">Info at a glance:</p>
 			<div class="row">
-				<div class="col">
-					<a href="#" class="badge badge-secondary">Tag</a>
-					<a href="#" class="badge badge-secondary">Tag</a>
-					<a href="#" class="badge badge-secondary">Tag</a>
-					<a href="#" class="badge badge-secondary">Tag</a>
-					<a href="#" class="badge badge-secondary">Tag</a>
-					<a href="#" class="badge badge-secondary">Tag</a>
+				<div class="col text-center">
+					<a href="#" class="badge badge-secondary">tag name</a>
+					<a href="#" class="badge badge-secondary">tag name</a>
+					<a href="#" class="badge badge-secondary">tag name</a>
+					<a href="#" class="badge badge-secondary">tag name</a>
+					<a href="#" class="badge badge-secondary">tag name</a>
+					<a href="#" class="badge badge-secondary">tag name</a>
+					<a href="#" class="badge badge-secondary">tag name</a>
 				</div>
 			</div>
 		</div>
