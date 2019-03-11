dc72e50b04cafbe7372da476e35b82319f67f94e
Scott Wells <efwells101@gmail.com>
2019-03-11 15:05:40 -0600

    finished homeview component

---
+++ static-ui/homeview.php
@@ -0,0 +1,13 @@
+<?php require_once ("head-utils.php");?>
+<?php require_once ("navbar.php");?>
+
+<section class="container-fluid">
+	<div class="row">
+		<div class="col-4">
+			<?php require_once ("trail-list.php");?>
+		</div>
+		<div class="col-8">
+			<img class="img-fluid" src="https://via.placeholder.com/1200x800">
+		</div>
+	</div>
+</section>
