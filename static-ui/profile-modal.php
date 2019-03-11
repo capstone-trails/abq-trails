d012f0c9d131f79313f100e7f666116e94740460
Ronald Luna <ronaldluna1@gmail.com>
2019-03-11 11:01:34 -0600

    setting up profile-modal.php https://app.asana.com/0/1108517459409227/1108517459409235

--- static-ui/profile-modal.php
+++ static-ui/profile-modal.php
@@ -1,44 +1,26 @@
 <?php require_once ("head-utils.php");?>
 
-<?php require_once("navbar.php");?>
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	<a class="navbar-brand" href="#">Navbar</a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
-
-	<div class="collapse navbar-collapse" id="navbarSupportedContent">
-		<ul class="navbar-nav mr-auto">
-			<li class="nav-item active">
-				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Link</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link disabled" href="#">Disabled</a>
-			</li>
-		</ul>
-		<form class="form-inline my-2 my-lg-0">
-			<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-		</form>
-	</div>
-</nav>
-
 
 <main>
 	<div class="container-fluid">
-		<h1 class="text-center">Update Profile</h1>
-		<form class="form-control-lg" id="form" action="" method="post">
-			<div class="info">
-				<input class="form-control" id="email" type="email" name="email" placeholder=" Email"/>
-				<input class="form-control" id="password" type="text" name="password" placeholder=" Password"/>
-				<input class="form-control" id="password-confirm" type="text" name="password-confirm" placeholder=" Re-enter Password"/>
+		<img src="https://via.placeholder.com/150" class="rounded mx-auto d-block" alt="...">
+		<h1 class="text-center">Profile Name</h1>
+
+		<h2 align="center">My Photos</h2>
+		<div class="row">
+			<div class="col-md-3 col-sm-4 col-xs-6">
+				<a href="{site_url}scents/baobab/pearls/black-pearls"><img src="https://via.placeholder.com/150"></a>
 			</div>
-			<button type="button" class="btn pull-right m-2" data-dismiss="modal">Cancel</button>
-			<button type="button" class="btn green pull-right m-2">Save changes</button>
-		</form>
+			<div class="col-md-3 col-sm-4 col-xs-6">
+				<a href="{site_url}scents/baobab/pearls/black-pearls"><img src="https://via.placeholder.com/150"></a>
+			</div>
+			<div class="col-md-3 col-sm-4 col-xs-6">
+				<a href="{site_url}scents/baobab/pearls/black-pearls"><img src="https://via.placeholder.com/150"></a>
+			</div>
+			<div class="col-md-3 col-sm-4 col-xs-6">
+				<a href="{site_url}scents/baobab/pearls/black-pearls"><img src="https://via.placeholder.com/150"></a>
+			</div>
+		</div>
+
 	</div>
 </main>
