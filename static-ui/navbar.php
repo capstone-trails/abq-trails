9cf9c2c89cbda801d119d4340276eb86e1b1768d
Cassandra Romero <romero.cassandra1@gmail.com>
2019-03-11 13:29:33 -0600

    nav bar

--- static-ui/navbar.php
+++ static-ui/navbar.php
@@ -1,37 +1,43 @@
 <?php require_once ("head-utils.php");?>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	<a class="navbar-brand" href="#"><i class="fas fa-mountain"></i></a>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm-6">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					<a class="navbar-brand" href="#"><i class="fas fa-mountain"></i></a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
 
-	<form class="form-inline ">
-		<input class="form-control" type="search" placeholder="Search" aria-label="Search">
-		<button class="btn" type="submit"><i class="fas fa-search"></i></button>
-	</form>
-
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
-
-	<div class="collapse navbar-collapse" id="navbarSupportedContent">
-		<ul class="navbar-nav ml-auto">
-			<li class="nav-item active">
-				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Trails</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Photos</a>
-			</li>
-			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					<i class="fas fa-user"></i>
-				</a>
-				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-					<a class="dropdown-item" href="#">Login</a>
-					<a class="dropdown-item" href="#">Sign Out</a>
-				</div>
-			</li>
-		</ul>
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav">
+							<li class="nav-item active">
+								<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Trails</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Photos</a>
+							</li>
+							<li class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									<i class="fas fa-user"></i>
+								</a>
+								<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+									<a class="dropdown-item" href="#">Login</a>
+									<a class="dropdown-item" href="#">Sign Out</a>
+								</div>
+							</li>
+						</ul>
+					</div>
+		</div>
+					<div class="col-sm-6">
+						<form class="form-inline">
+							<input class="form-control" type="search" placeholder="Search" aria-label="Search">
+							<button class="btn" type="submit"><i class="fas fa-search"></i></button>
+						</form>
+					</div>
+			</nav>
 	</div>
-</nav>
+</div>
