8d592619bb2805ced7d827dbd202e211097d03b9
Robert Dominguez <dominguez.1st@gmail.com>
2019-03-12 15:07:44 -0600

    Made some changes to profile

--- ng/src/app/shared/services/profile.service.ts
+++ ng/src/app/shared/services/profile.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpParams} from "@angular/common/http";
-import {Status} from "../classes/status";
-import {Profile} from "../classes/profile";
+import {Status} from "../interfaces/status";
+import {Profile} from "../interfaces/profile";
 import {Observable} from "rxjs/Observable";
 
 @Injectable ()
@@ -11,13 +11,8 @@ export class ProfileService  {
 
 	}
 
-	//define the API endpoint test test
+	//define the API endpoint
 	private profileUrl = "api/profile/";
-
-	//reach out to the profile  API and delete the profile in question
-	deleteProfile(id : string) : Observable<Status> {
-		return(this.http.delete<Status>(this.profileUrl + id));
-	}
 
 	// call to the Profile API and edit the profile in question
 	editProfile(profile: Profile) : Observable<Status> {
@@ -27,15 +22,5 @@ export class ProfileService  {
 	// call to the Profile API and get a Profile object by its id
 	getProfileByProfileId(id: string) : Observable<Profile> {
 		return(this.http.get<Profile>(this.profileUrl + id));
-	}
-
-	// call to the API to grab an array of profiles based on the user input
-	getProfileByProfileUserName(profileUserName: string) :Observable<Profile[]> {
-		return(this.http.get<Profile[]>(this.profileUrl, {params: new HttpParams().set("profileUserName", profileUserName)}));
-	}
-
-	//call to the profile API and grab the corresponding profile by its email
-	getProfileByProfileEmail(profileEmail: string) :Observable<Profile[]> {
-		return(this.http.get<Profile[]>(this.profileUrl, {params: new HttpParams().set("profileEmail", profileEmail)}));
 	}
 }
