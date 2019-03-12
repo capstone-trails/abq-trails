a66bb14e00a2ed8933fe4456f389ae4c3043b5a9
Scott Wells <efwells101@gmail.com>
2019-03-12 16:32:40 -0600

    fixed parameters for rating, trail, sign-in, sign-up

--- ng/src/app/shared/services/rating.service.ts
+++ ng/src/app/shared/services/rating.service.ts
@@ -1,10 +1,8 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Status} from "../interfaces/status";
-import {Observable} from "rxjs/internal/Observable";
-import {Profile} from "../interfaces/profile";
 import {Rating} from "../interfaces/rating";
-import {Trail} from "../interfaces/trail";
+import {Observable} from "rxjs/internal/Observable";
 
 
 @Injectable()
@@ -13,10 +11,10 @@ export class RatingService {
 	constructor(protected http : HttpClient) {}
 
 	//define the API endpoint
-	private ratingUrl = "https://bootcamp-coders.cnm.edu/~swells19/abq-trails/public_html/api/rating/";
+	private ratingUrl = "/api/rating/";
 
 	createRating(rating : Rating) : Observable<Status> {
-		return(this.http.post<Status>(this.ratingUrl + rating));
+		return(this.http.post<Status>(this.ratingUrl, rating));
 	}
 
 	getRatingByProfileIdAndTrailId(ratingProfileId : string, ratingTrailId : string) : Observable<Rating> {
--- ng/src/app/shared/services/sign-in.service.ts
+++ ng/src/app/shared/services/sign-in.service.ts
@@ -1,31 +1,25 @@
-
 import {Injectable} from "@angular/core";
-
-
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {Status} from "../interfaces/status";
 import {SignIn} from "../interfaces/sign-in";
-
-
 import {Observable} from "rxjs/internal/Observable";
-import {HttpClient} from "@angular/common/http";
 
 @Injectable()
-export class SignUpService {
+export class SignInService {
 
 	constructor(protected http : HttpClient) {}
 
 	//define the API endpoint
-	private apiUrl = "https://bootcamp-coders.cnm.edu/~swells19/abq-trails/public_html/api/";
+	private signInUrl = "/api/sign-in/";
+	private signOutUrl = "/api/sign-out/";
 
 
-	// Sign-In API -- POST
 	postSignIn(signIn : SignIn) : Observable<Status> {
-		return(this.http.post<Status>(this.apiUrl, signIn));
+		return(this.http.post<Status>(this.signInUrl, signIn));
 	}
 
-	//Sign-Out API -- GET
-	getSignOut(signOut : SignOut) : Observable<Status> {
-		return(this.http.get<Status>(this.apiUrl, signOut))
+	getSignOut() : Observable<Status> {
+		return(this.http.get<Status>(this.signOutUrl));
 	}
 
 }
