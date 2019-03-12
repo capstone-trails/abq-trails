5f951307efeae7d717689ef294683a8ebca5b17d
Robert Dominguez <dominguez.1st@gmail.com>
2019-03-12 14:53:06 -0600

    Close to done with profile

--- ng/src/app/shared/services/profile.service.ts
+++ ng/src/app/shared/services/profile.service.ts
@@ -1,21 +1,41 @@
-
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import  {Profile} from "../interfaces/profile";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Status} from "../classes/status";
+import {Profile} from "../classes/profile";
+import {Observable} from "rxjs/Observable";
 
-@Injectable()
-export class ApiService {
+@Injectable ()
+export class ProfileService  {
 
-	constructor(protected http : HttpClient) {}
+	constructor(protected http : HttpClient) {
 
-	//define the API endpoint
-	private apiUrl = "https://bootcamp-coders.cnm.edu/~rdominguez45/abq-trails/public_html/api/";
-
-	// Sign-Up API -- POST
-	postSignUp(signUp : SignUp) : Observable<Status> {
-		return(this.http.post<Status>(this.apiUrl, signUp));
 	}
 
-	// Sign-In API -- POST
-	postSignIn(signIn : )
+	//define the API endpoint
+	private profileUrl = "api/profile/";
+
+	//reach out to the profile  API and delete the profile in question
+	deleteProfile(id : string) : Observable<Status> {
+		return(this.http.delete<Status>(this.profileUrl + id));
+	}
+
+	// call to the Profile API and edit the profile in question
+	editProfile(profile: Profile) : Observable<Status> {
+		return(this.http.put<Status>(this.profileUrl, profile));
+	}
+
+	// call to the Profile API and get a Profile object by its id
+	getProfileByProfileId(id: string) : Observable<Profile> {
+		return(this.http.get<Profile>(this.profileUrl + id));
+	}
+
+	// call to the API to grab an array of profiles based on the user input
+	getProfileByProfileUserName(profileUserName: string) :Observable<Profile[]> {
+		return(this.http.get<Profile[]>(this.profileUrl, {params: new HttpParams().set("profileUserName", profileUserName)}));
+	}
+
+	//call to the profile API and grab the corresponding profile by its email
+	getProfileByProfileEmail(profileEmail: string) :Observable<Profile[]> {
+		return(this.http.get<Profile[]>(this.profileUrl, {params: new HttpParams().set("profileEmail", profileEmail)}));
+	}
 }
