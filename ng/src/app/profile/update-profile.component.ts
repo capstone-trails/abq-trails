185f0a911b2c0a0d8a20f9f46a485a7360378d9c
Cassandra Romero <romero.cassandra1@gmail.com>
2019-03-13 14:24:39 -0600

    update profile component added

---
+++ ng/src/app/profile/update-profile.component.ts
@@ -0,0 +1,26 @@
+/*
+ this component is for signing up to use the site.
+ */
+
+//import needed modules for the sign-up component
+import {Component, OnInit, ViewChild,} from "@angular/core";
+import {Router} from "@angular/router";
+import {Status} from "../shared/interfaces/status"
+import {Profile} from "../shared/interfaces/profile";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ProfileService} from "../shared/services/profile.service";
+
+@Component({
+	templateUrl:"./update-profile.component.html"
+})
+
+//set template URL and the selector for the ng powered html
+
+export class UpdateProfileComponent implements OnInit {
+
+	updateProfileForm : FormGroup;
+	status: Status = {status: null, message: null, type: null};
+
+	constructor(private formBuilder : FormBuilder, private profileService : ProfileService){
+	}
+}
