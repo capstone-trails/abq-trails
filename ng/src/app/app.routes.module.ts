ad5442f1410ed3ea58271d6e84a4a31f1d586978
Scott Wells <efwells101@gmail.com>
2019-03-14 10:23:53 -0600

    create sign-in shared component template directory

--- ng/src/app/app.routes.module.ts
+++ ng/src/app/app.routes.module.ts
@@ -1,7 +1,6 @@
 import {RouterModule, Routes} from "@angular/router";
 import {SplashComponent} from "./splash/splash.component";
-import{AppComponent} from "./app.component"
-
+import{AppComponent} from "./app.component";
 import {SignUpComponent} from "./sign-up/sign-up.component";
 import {ProfileService} from "./shared/services/profile.service";
 import {RatingService} from "./shared/services/rating.service";
@@ -11,17 +10,23 @@ import {TagService} from "./shared/services/tag.service";
 import {TrailtagService} from "./shared/services/trailtag.service";
 import {TrailService} from "./shared/services/trail.service";
 import {SessionService} from "./shared/services/session.services";
+import {SignInComponent} from "./sign-in/sign-in.component";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {DeepDiveInterceptor} from "./shared/interceptors/interceptor";
 
 
-export const allAppComponents = [AppComponent, SignUpComponent, SplashComponent];
+export const allAppComponents = [AppComponent, SignUpComponent, SignInComponent, SplashComponent];
 
 export const routes: Routes = [
 	{path: "sign-up", component: SignUpComponent},
+	{path: "sign-in", component: SignInComponent},
 	{path: "", component: SplashComponent}
 
 ];
 
-export const appRoutingProviders: any[] = [ProfileService, RatingService, SignInService, SignUpService, TagService, TrailtagService, TrailService, SessionService
+export const appRoutingProviders: any[] = [
+	ProfileService, RatingService, SignInService, SignUpService, TagService, TrailtagService, TrailService, SessionService,
+	{provide: HTTP_INTERCEPTORS, useClass: DeepDiveInterceptor, multi: true}
 ];
 
 export const routing = RouterModule.forRoot(routes);
--- ng/src/app/sign-in/sign-in.component.ts
+++ ng/src/app/sign-in/sign-in.component.ts
@@ -1,37 +1,66 @@
-/*
- this component is for signing up to use the site.
- */
 
-//import needed modules for the sign-up component
 import {Component, OnInit, ViewChild,} from "@angular/core";
 import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
 import {Status} from "../shared/interfaces/status"
 import {SignIn} from "../shared/interfaces/sign-in";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {CookieService} from "ng2-cookies";
+
 import {SignInService} from "../shared/services/sign-in.service";
+import {SessionService} from "../shared/services/session.services";
 
 
 @Component({
-	templateUrl:"./sign-in.component.html"
+	templateUrl: "./sign-in.component.html",
+	selector: "sign-in"
 })
 
-//set template URL and the selector for the ng powered html
 
-export class SignInComponent {
+export class SignInComponent implements OnInit {
 
+	SignInForm : FormGroup;
 
-	signInForm : FormGroup;
-	status : Status = {status: null, message: null, type: null};
+	signIn : SignIn = new SignIn (null, null);
+	status : Status = null;
 
-	constructor(private formBuilder : FormBuilder, private signInService : SignInService, private router: Router, private cookieService: CookieService){
+	constructor(
+		private formBuilder : FormBuilder,
+		private router : Router,
+		private signInService : SignInService,
+		private sessionService : SessionService
+	) {}
+
+	ngOnInit() : void {
+		this.signInForm = this.formBuilder.group({
+				profileEmail: ["", [Validators.maxLength(128), Validators.required]],
+				profilePassword: ["", [Validators.maxLength(32), Validators.required]]
+			}
+		);
+		this.applyFormChanges();
 	}
 
-	signIn() : void {
-		localStorage.removeItem("jwt-token");
-		this.signInService.postSignIn(this.signIn).subscribe(status => {
-			this.status = status;
+	applyFormChanges() : void {
+		this.signInForm.valueChanges.subscribe(values => {
+			for(let field in values) {
+				this.signIn[field] = values[field];
+			}
 		});
+	}
+
+	createSignIn(): void {
+		this.signInService.postSignIn(this.signIn)
+			.subscribe(status => {
+				this.status = status;
+
+				if(this.status.status === 200) {
+					this.sessionService.setSession();
+					this.signInForm.reset();
+					location.reload();
+				}
+			});
+	}
+	signOut() : void {
+		this.signInService.getSignOut();
 	}
 
 }
