a135b205c3d444580f17fbf60fa93b12ad67f52d
Scott Wells <efwells101@gmail.com>
2019-03-15 21:38:00 -0600

    added mapbox module to project

--- ng/src/app/app.component.ts
+++ ng/src/app/app.component.ts
@@ -1,18 +1,51 @@
 import { Component } from '@angular/core';
 import { SessionService } from "./shared/services/session.services";
 import  { Status } from "./shared/interfaces/status";
+import { MapService } from "ngx-mapbox-gl";
+import { faPaw } from "@fortawesome/free-solid-svg-icons";
 
 
 @Component({
   selector: 'app-root',
-	templateUrl: './app.component.html'
+	templateUrl: './app.component.html',
+	styles: [`
+		mgl-map {
+			height: 80vh;
+			width: 80vw;
+		}
+		mgl-marker {
+			width: 15px;
+			height: 15px;
+			border-radius: 50%;
+			background-color: red;
+		}
+	`],
 })
+
+
 export class AppComponent {
 
 	status : Status = null;
 
-	constructor(protected sessionService : SessionService) {
+	Geo
+	map: any;
+	lat: number = 35.0856181;
+	lng: number = -106.6493357;
+	fawPaw = faPaw;
+
+
+	points: any = [
+		{name: "one", lng: -106.6495457, lat: 35.0356181},
+		{name: "two", lng: -106.6503457, lat: 35.0556161},
+		{name: "three", lng: -106.6513557, lat: 35.0756131},
+		{name: "two", lng: -106.6493757, lat: 35.0956141},
+	];
+
+	constructor(protected sessionService : SessionService, private mapService: MapService) {
 		this.sessionService.setSession()
 			.subscribe(status => this.status = status);
 	}
+
+
+
 }
--- ng/src/app/app.module.ts
+++ ng/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import {ReactiveFormsModule} from "@angular/forms";
-import {JwtModule} from "@auth0/angular-jwt";
-import {allAppComponents, appRoutingProviders, routing} from "./app.routes.module";
+import { ReactiveFormsModule } from "@angular/forms";
+import { JwtModule } from "@auth0/angular-jwt";
+import { allAppComponents, appRoutingProviders, routing } from "./app.routes.module";
 import { AppComponent } from './app.component'
+import { MapService, NgxMapboxGLModule } from "ngx-mapbox-gl";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
+
+let ngxMapboxConfig = NgxMapboxGLModule.withConfig({
+	accessToken: 'pk.eyJ1IjoiZ2Vvcmdla2VwaGFydCIsImEiOiJjanQ4cmdmYjkwYnZnNDNwNDF4NXFiMTJmIn0.MwDDiyszR0QFmMYMNvzi1Q',
+});
 
 const JwtHelper = JwtModule.forRoot({
 	config : {
@@ -21,9 +27,10 @@ const JwtHelper = JwtModule.forRoot({
 });
 
 @NgModule({
-	imports:      [ BrowserModule, FormsModule, HttpClientModule, routing, ReactiveFormsModule, JwtHelper],
+	imports:      [ BrowserModule, FormsModule, HttpClientModule, routing, ReactiveFormsModule, JwtHelper, ngxMapboxConfig, FontAwesomeModule, ],
 	declarations: [ ...allAppComponents, AppComponent],
 	bootstrap:    [ AppComponent ],
-	providers:    [appRoutingProviders],
+	providers:    [ appRoutingProviders, MapService, ],
 })
+
 export class AppModule { }
