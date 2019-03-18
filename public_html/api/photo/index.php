f87c94317ff9ec3fee8416119ca9f52fa6918f82
Scott Wells <efwells101@gmail.com>
2019-03-18 16:01:44 -0600

    created cloudinary photo api

--- public_html/api/photo/index.php
+++ public_html/api/photo/index.php
@@ -43,13 +43,20 @@ try {
 
 	\Cloudinary::config(["cloud_name" => $cloudinary->cloudName, "api_key" => $cloudinary->apiKey, "api_secret" => $cloudinary->apiSecret]);
 
-
 	//process GET requests
 	if($method === "GET") {
 		setXsrfCookie();
-		$reply->data = getPhotoByTrailId
+		$reply->data = photo::getPhotoByPhotoId($pdo)->toArray();
+	} else if($method === "POST") {
+		verifyXsrf();
+		$photoId = filter_input(INPUT_POST, "photoId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		$tempUserFileName = $_FILES["image"]["temp_name"];
+		$cloudinaryResult = \Cloudinary\Uploader::upload($tempUserFileName, array("width" => 200, "crop" => "scale"));
+		$image = new Image(generateUuidV4(), $photoId, $cloudinaryResult["signature"], $cloudinaryResult["secure_url"]);
+		$image->insert($pdo);
+		var_dump($image);
+		$reply->message = "Image uploaded!";
 	}
-
 
 } catch(Exception $exception) {
 	$reply->status = $exception->getCode();
