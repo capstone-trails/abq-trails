6b7960d5336a3671b4bfffc23bd362c500a11330
Scott Wells <efwells101@gmail.com>
2019-03-20 16:47:44 -0600

    added ngIf to navbar

--- ng/src/app/app.component.ts
+++ ng/src/app/app.component.ts
@@ -5,6 +5,7 @@ import {SignInService} from "./shared/services/sign-in.service";
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import {Router} from "@angular/router";
 import {SignInComponent} from "./sign-in/sign-in.component";
+import {AuthService} from "./shared/services/auth-service";
 
 
 
@@ -16,15 +17,18 @@ import {SignInComponent} from "./sign-in/sign-in.component";
 
 export class AppComponent {
 
+	isLoggedIn: boolean;
+
 	status : Status = null;
 
-	constructor(private sessionService : SessionService, private signInService: SignInService, private router: Router, private modalService: NgbModal) {
+	constructor(private authService : AuthService, private sessionService : SessionService, private signInService: SignInService, private router: Router, private modalService: NgbModal) {
 		this.sessionService.setSession()
 			.subscribe(status => this.status = status);
+		this.isLoggedIn = this.authService.loggedIn();
 	}
 
 	signOut() : void {
-		this.signInService.getSignOut();
+		this.signInService.getSignOut().subscribe( reply => reply );
 		window.localStorage.clear();
 		location.reload();
 		this.router.navigate([""]);
